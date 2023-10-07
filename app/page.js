cb58ded9dbaaeecffaa2ee45fd2d0349de85ede1
Anubhav Singh <anubhav@mail.craftindika.com>
2023-10-07 19:53:15 +0530

    fix

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import Email from "@/components/EnterEmail";
-import Footer from "@/components/Footer";
+import Email from '@/components/EnterEmail';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
     <div className=" relative z-10 flexBetween flex-col w-full h-screen  pt-5   overflow-x-hidden">
       <Email />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
